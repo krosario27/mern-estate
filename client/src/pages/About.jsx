0a19a3fb7b88413b12ca257e170f803e5a8fc613
Kyle <kylewhite369@gmail.com>
2025-03-06 09:04:12 +1300

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-yellow-800'>About Kyle Estate</h1>
+      <p className='mb-4 text-slate-700'>At Kyle Estate, we make finding your perfect home effortless. 
+        Whether you’re searching for a place to buy, rent, or invest in, 
+        our platform offers a wide range of properties to suit every lifestyle 
+        and budget. With advanced search tools, expert advice, and up-to-date 
+        listings, we ensure a seamless experience from browsing to closing the deal.
+      </p>
+
+      <p className='mb-4 text-slate-700'>
+      Our mission is to connect buyers, sellers, and renters with their ideal properties 
+      while providing transparency, trust, and convenience every step of the way. 
+      Whether you're a first-time buyer, a seasoned investor, or someone looking for a 
+      temporary rental, Kyle Estate simplifies the process, making your real estate 
+      journey smooth and successful.
+      </p>
+
+    </div>
   )
 }
